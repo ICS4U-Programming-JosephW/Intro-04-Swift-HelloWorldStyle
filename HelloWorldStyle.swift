63ec21d7e64f5d8be15bd22eb21539a5b9af7d24
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-09-09 15:31:39 +0000

    Added HelloWorldStyle.swift.

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,15 @@
+//
+//  HelloWorldStyle.swift
+//
+//  Created by Joseph Wondimagnehu
+//  Created on 2025-09-09
+//  Version 2.0
+//  Copyright (c) 2025 Joseph Wondimagnehu. All rights reserved.
+//
+//  This program focuses on style and
+//  comments for the greeting program.
+
+// Greets Beni.
+print("\nHello, Beni! With style!")
+// Outputs an additional statement.
+print("Java is the superior language! In my opinion, of course.\n")
